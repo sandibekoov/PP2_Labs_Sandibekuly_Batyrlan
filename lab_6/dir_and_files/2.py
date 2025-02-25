356f56da918d0bd1dd7ac3b921aa248411ff7657
sandibekoov <b_sandibekuly@kbtu.kz>
2025-02-25 11:24:32 +0500

    Lab 6.1

--- lab_6/dir_and_files/2.py
+++
@@ -1,8 +0,0 @@
-import os
-
-path = input()
-
-print("Exists:", os.path.exists(path))
-print("Readable:", os.access(path, os.R_OK))
-print("Writable:", os.access(path, os.W_OK))
-print("Executable:", os.access(path, os.X_OK))
--- lab_6/dir_and_files/4.py
+++
@@ -1,8 +0,0 @@
-file_path = input()
-
-try:
-    with open(file_path, "r", encoding="utf-8") as file:
-        lines = file.readlines()
-        print("Number of lines:", len(lines))
-except FileNotFoundError:
-    print("File not found.")
